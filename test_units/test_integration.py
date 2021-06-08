b9400236947b5437fb030c5f46c2486da63d54cd
John Gregg <greggj2016@gmail.com>
2021-06-08 12:11:35 -0400

    second integration test

--- test_units/test_integration.py
+++ test_units/test_integration.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import pdb
-
 import unittest
 import mock
 import numpy as np
@@ -32,25 +31,38 @@ os.chdir(paths[count - 1])
 sys.path.insert(1, os.getcwd())
 from remove_outliers_library import remove_all_outliers
 
+def bootstrap_mean(x):
+    np.random.choice(x, (len(x), 100000))
+
 class test_main_library(unittest.TestCase):
     np.random.seed(0)
 
     def test_outlier_removal(self):
         raw_data = pd.read_csv("all_2018_processed.txt",
-                               delimiter = "\t", header = 0)
-        clean_data = remove_all_outliers("all_2018_processed.txt")[0]
-        count_correctness = []
-        for colname in np.array(raw_data.columns):
-            raw_col = np.round(raw_data[colname], 6)
-            clean_col = np.round(clean_data[colname], 6)
-            lb, ub = np.min(clean_col), np.max(clean_col)
-            is_inlier = np.logical_and(raw_col <= ub, raw_col >= lb)
-            clean_count = np.sum(np.isnan(clean_col) == False)
-            raw_count = np.sum(np.isnan(raw_col[is_inlier]) == False)
-            count_correctness.append(clean_count == raw_count)
+                               delimiter = "\t", header = 0).to_numpy()
+        new_data = remove_all_outliers("all_2018_processed.txt")[0].to_numpy()
+        counts = np.array([len(np.unique(col[np.isnan(col) == False]))
+                           for col in raw_data.T])
+        raw_data = raw_data[:, counts >= 10]
+        new_data = new_data[:, counts >= 10]
+        lbs = np.nanmin(new_data, axis = 0)
+        ubs = np.nanmax(new_data, axis = 0)
+        is_inlier = np.logical_and(raw_data <= ubs, raw_data >= lbs)
+        raw_counts = np.sum(is_inlier, axis = 0)
+        new_counts = np.sum(np.isnan(new_data) == False, axis = 0)
         error_message = "Not all outliers have been removed and/or "
         error_message += "non-outlier points have been removed"
-        self.assertTrue(np.all(count_correctness), error_message)
+        self.assertTrue(np.all(raw_counts == new_counts), error_message)
+
+        old_counts = np.sum(np.isnan(raw_data) == False, axis = 0)
+        percents = new_counts/old_counts
+        bootstraps = np.random.choice(percents, (len(percents), 1000000))
+        bs_means = np.mean(bootstraps, axis = 0)
+        CI = np.percentile(bs_means, [2.5, 97.5])
+        good_CI = np.array([0.99406369, 0.99529648])
+        error_message = "The confidence interval for percent of data removed"
+        error_message += " is not correct. There may be a mathematical error."
+        self.assertTrue(np.all(np.isclose(CI, good_CI)), error_message)
         
 if __name__ == '__main__':
     unittest.main()
